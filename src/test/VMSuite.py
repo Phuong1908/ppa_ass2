ad0fa0723e93e8536884c88c64d3113fe116c49c
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2024-04-08 22:54:11 +0700

    [f] Fix test case

--- src/test/VMSuite.py
+++ src/test/VMSuite.py
@@ -3,23 +3,22 @@ from TestUtils import TestVM
 
 
 class VMSuite(unittest.TestCase):
-    def test_simple_program(self):        
+    def test_simple_program(self):
         input = """[[],[],[call(writeInt,[3])]]."""
         expect = "3"
         self.assertTrue(TestVM.test(input, expect, 401))
 
-    def test_simple_expression(self):        
+    def test_simple_expression(self):
         input = """[[],[],[call(writeInt,[add(3,1)])]]."""
         expect = "4"
         self.assertTrue(TestVM.test(input, expect, 402))
 
-    def test_redeclaration(self):        
+    def test_redeclaration(self):
         input = """[[var(a,integer),var(b,integer),var(a,real)],[],[call(writeInt,[1])]]."""
         expect = "Redeclared identifier: var(a,real)"
         self.assertTrue(TestVM.test(input, expect, 403))
 
-    def test_minus_expression(self):        
-        input = """[[],[],[call(writeInt,[minus(3,1)])]]."""
+    def test_minus_expression(self):
+        input = """[[],[],[call(writeInt,[sub(3,1)])]]."""
         expect = "2"
         self.assertTrue(TestVM.test(input, expect, 404))
-
