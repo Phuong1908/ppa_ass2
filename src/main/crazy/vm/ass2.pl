b9008a775ec02e59de8af8d184b81c6208cd2c65
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2024-06-01 13:47:09 +0700

    [f] Refactor code and handling type mismatch

--- src/main/crazy/vm/ass2.pl
+++ src/main/crazy/vm/ass2.pl
@@ -1,11 +1,12 @@
-% main program
-go :- 	open('input.txt', read, Stream),
-        read_term(Stream, Y, []),
-        close(Stream),
-        open('output.txt', write, Stream1),
-        set_output(Stream1),
-        catch(reduce_prog(Y), Exception, process(Exception)),
-        close(Stream1).
+% Main program
+go :- 
+    open('input.txt', read, Stream),
+    read_term(Stream, Y, []),
+    close(Stream),
+    open('output.txt', write, Stream1),
+    set_output(Stream1),
+    catch(reduce_prog(Y), Exception, process(Exception)),
+    close(Stream1).
 
 reduce_prog([Var, Func, Body]) :-
     create_env(Var, env([], 0, 0), Env), % Init env from the global env
@@ -62,35 +63,34 @@ get_type_expression(_, bor(_, _), boolean).
 % Numerical expressions
 get_type_expression(Env, add(X, Y), integer) :- 
     get_type_expression(Env, X, integer),
-    get_type_expression(Env, Y, integer).
+    get_type_expression(Env, Y, integer), !.
 get_type_expression(Env, add(X, Y), float) :- 
     (get_type_expression(Env, X, float);
-    get_type_expression(Env, Y, float)).
+    get_type_expression(Env, Y, float)), !.
 get_type_expression(Env, sub(X, Y), integer) :- 
     get_type_expression(Env, X, integer),
-    get_type_expression(Env, Y, integer).
+    get_type_expression(Env, Y, integer), !.
 get_type_expression(Env, sub(X, Y), float) :- 
     (get_type_expression(Env, X, float);
-    get_type_expression(Env, Y, float)).
+    get_type_expression(Env, Y, float)), !.
 get_type_expression(Env, times(X, Y), integer) :- 
     get_type_expression(Env, X, integer),
-    get_type_expression(Env, Y, integer).
+    get_type_expression(Env, Y, integer), !.
 get_type_expression(Env, times(X, Y), float) :- 
     (get_type_expression(Env, X, float);
-    get_type_expression(Env, Y, float)).
+    get_type_expression(Env, Y, float)), !.
 get_type_expression(Env, rdiv(X, Y), integer) :- 
     get_type_expression(Env, X, integer),
-    get_type_expression(Env, Y, integer).
+    get_type_expression(Env, Y, integer), !.
 get_type_expression(Env, rdiv(X, Y), float) :- 
     (get_type_expression(Env, X, float);
-    get_type_expression(Env, Y, float)).
-
+    get_type_expression(Env, Y, float)), !.
 get_type_expression(Env, idiv(X, Y), integer) :- 
     get_type_expression(Env, X, integer),
-    get_type_expression(Env, Y, integer).
+    get_type_expression(Env, Y, integer), !.
 get_type_expression(Env, imod(X, Y), integer) :- 
     get_type_expression(Env, X, integer),
-    get_type_expression(Env, Y, integer).
+    get_type_expression(Env, Y, integer), !.
 
 % Relational expressions
 get_type_expression(Env, greater(X, Y), boolean) :-
@@ -120,25 +120,20 @@ rel_op(ne).
 rel_op(eql).
 
 % Type check assignment
-type_check_assignment(Env, integer, Y) :- get_type_expression(Env, Y, integer).
-type_check_assignment(Env, real, Y) :- get_type_expression(Env, Y, real).
-type_check_assignment(Env, string, Y) :- get_type_expression(Env, Y, string).
-type_check_assignment(Env, boolean, Y) :- get_type_expression(Env, Y, boolean).
+type_check_assignment(Env, T, Y, Var) :- 
+    (get_type_expression(Env, Y, T) -> true ; throw(type_mismatch(assign(Var, Y)))).
 
 % Type checking one statement
 type_check_stmt(Env, assign(X, Y)) :- 
     lookup(Env, X, id(X, _, T)),
-    type_check_assignment(Env, T, Y).
+    type_check_assignment(Env, T, Y, X), !.
 
-% Type checking for a call statement
-% Built-in functions
 type_check_stmt(Env, call(Func, [X])) :- 
     (is_write_func(Func); is_write_ln_func(Func)),
-    get_type_expression(Env, X, _).
+    get_type_expression(Env, X, _), !.
 
-% User-defined functions									
-% Type check one block										
-type_check_body(_, []).
+% Type check one block
+type_check_body(_, []) :- !.
 type_check_body(env(L, B, T), [var(X, Y) | _]) :- 
     has_declared(X, env(L, B, T)), !,
     throw(redeclare_identifier(var(X, Y))).
@@ -147,25 +142,25 @@ type_check_body(env(L, B, T), [var(X, Y) | L1]) :-
     type_check_body(env([id(X, var, Y) | L], B, T1), L1), !.
 type_check_body(Env, [X | L]) :- 
     type_check_stmt(Env, X), 
-    type_check_body(Env, L).
+    type_check_body(Env, L), !.
 
 % Type checking a procedure
 type_check_one_func(Env, proc(_, Y, Z)):- 
     create_env(Y, Env, env(L, _, T)),
-	type_check_body(env(L, T, T), Z).
+    type_check_body(env(L, T, T), Z), !.
 
 % Type checking a list of procedures
-type_check_func(Env, [], Env).
+type_check_func(Env, [], Env) :- !.
 type_check_func(_, [proc(X, _, _) | _], _) :- 
     is_builtin(X), !,
     throw(redeclare_procedure(X)).
-type_check_func(Env, [proc(X, _, _) | _], _) :- 
-    has_declared(X, Env), !,
+type_check_func(_, [proc(X, _, _) | _], _) :- 
+    has_declared(X, _), !,
     throw(redeclare_procedure(X)).
 type_check_func(env(Env, B, T), [proc(X, Y, Z) | L], Env1) :- 
     T1 is T + 1,
     type_check_one_func(env([id(X, proc, proc(Y, Z)) | Env], T1, T1), proc(X, Y, Z)), !,
-    type_check_func(env([id(X, proc, proc(Y, Z)) | Env], B, T1), L, Env1).
+    type_check_func(env([id(X, proc, proc(Y, Z)) | Env], B, T1), L, Env1), !.
 
 % Check if X has been declared in the symbol table from B+1 to T
 has_declared(X, env([id(X, _, _) | _], B, T)) :- 
@@ -194,7 +189,7 @@ update_var(I, V, env(L, B, T), env(L1, B, T)) :-
 update_var_list(I, _, [], _) :- throw(undeclare_identifier(I)).
 update_var_list(I, V, [id(I, Type, _) | L], [id(I, Type, V) | L]) :- !.
 update_var_list(I, V, [H | L], [H | L1]) :-
-    update_var_list(I, V, L, L1).
+    update_var_list(I, V, L, L1), !.
 
 % Print the environment for debugging
 print_env(env(L, _, _)) :-
@@ -204,7 +199,7 @@ print_env(env(L, _, _)) :-
 print_env_list([]).
 print_env_list([id(X, Type, V) | L]) :-
     format("~w: ~w = ~w~n", [Type, X, V]),
-        print_env_list(L).
+    print_env_list(L), !.
 
 is_builtin(readInt).
 is_builtin(writeIntLn).
@@ -239,7 +234,7 @@ create_runtime_env(X, X).
 reduce_arith(config(Op, E1, E2), Env, R) :-
     reduce_all(config(E1, Env), config(V1, Env)),
     reduce_all(config(E2, Env), config(V2, Env)),
-    arith_op(Op, V1, V2, R).
+    arith_op(Op, V1, V2, R), !.
 
 arith_op(add, V1, V2, R) :- R is V1 + V2.
 arith_op(sub, V1, V2, R) :- R is V1 - V2.
@@ -251,85 +246,83 @@ arith_op(imod, V1, V2, R) :- R is mod(V1, V2).
 % Reduce for unary subtraction
 reduce(config(sub(E1), Env), config(R, Env)) :-
     reduce_all(config(E1, Env), config(V1, Env)),
-    R is -V1.
+    R is -V1, !.
 
 % Specific reduce predicates using the general one
 reduce(config(add(E1, E2), Env), config(R, Env)) :-
-    reduce_arith(config(add, E1, E2), Env, R).
+    reduce_arith(config(add, E1, E2), Env, R), !.
 
 reduce(config(sub(E1, E2), Env), config(R, Env)) :-
-    reduce_arith(config(sub, E1, E2), Env, R).
+    reduce_arith(config(sub, E1, E2), Env, R), !.
 
 reduce(config(times(E1, E2), Env), config(R, Env)) :-
-    reduce_arith(config(times, E1, E2), Env, R).
+    reduce_arith(config(times, E1, E2), Env, R), !.
 
 reduce(config(rdiv(E1, E2), Env), config(R, Env)) :-
-    reduce_arith(config(rdiv, E1, E2), Env, R).
+    reduce_arith(config(rdiv, E1, E2), Env, R), !.
 
 reduce(config(idiv(E1, E2), Env), config(R, Env)) :-
-    reduce_arith(config(idiv, E1, E2), Env, R).
+    reduce_arith(config(idiv, E1, E2), Env, R), !.
 
 reduce(config(imod(E1, E2), Env), config(R, Env)) :-
-    reduce_arith(config(imod, E1, E2), Env, R).
+    reduce_arith(config(imod, E1, E2), Env, R), !.
 
 reduce(config(I, Env), config(V, Env)) :-
     atom1(I),
-    lookup(Env, I, id(I, _, V)).
+    lookup(Env, I, id(I, _, V)), !.
 
-reduce(config(E, Env), config(E, Env)).
+reduce(config(E, Env), config(E, Env)) :- !.
 
 % Reduce all expressions in the list until there is no expression to reduce
 reduce_all(config(V, Env), config(V, Env)) :- number(V), !.
 reduce_all(config(V, Env), config(V, Env)) :- boolean(V), !.
 reduce_all(config(str(V), Env), config(V, Env)) :- !.
 reduce_all(config(E, Env), config(E2, Env)) :-
-    reduce(config(E, Env), config(E1, Env)), !,
-    reduce_all(config(E1, Env), config(E2, Env)).
+    reduce(config(E, Env), config(E1, Env)),
+    reduce_all(config(E1, Env), config(E2, Env)), !.
 
 % Reduce a list of expressions
-reduce_all(config([], Env), config([], Env)).
+reduce_all(config([], Env), config([], Env)) :- !.
 reduce_all(config([S | L], Env), config([S1 | L1], Env1)) :-
     reduce_all(config(S, Env), config(S1, Env)),
-    reduce_all(config(L, Env), config(L1, Env1)).
+    reduce_all(config(L, Env), config(L1, Env1)), !.
 
 % Reduce a list of statements (body)
-reduce_stmt(config([], Env), Env).
+reduce_stmt(config([], Env), Env) :- !.
 reduce_stmt(config([var(X, Type) | Stmts], Env), Env1) :- % Handle variable declarations
     ( has_declared(X, Env) ->
         throw(redeclare_identifier(var(X, Type)))
     ; NewEnv = env([id(X, var, Type) | Env], 0, 0), % Add the variable to the environment
       reduce_stmt(config(Stmts, NewEnv), Env1)
-    ).
+    ), !.
 reduce_stmt(config([Stmt | Stmts], Env), Env1) :-
     reduce_stmt(config(Stmt, Env), Env2),
-    reduce_stmt(config(Stmts, Env2), Env1).
+    reduce_stmt(config(Stmts, Env2), Env1), !.
 
 % Reduce an if statement
-
 reduce_stmt(config(if(E, S1), Env), Env1) :-
     reduce_all(config(E, Env), config(V, Env)),
     (boolean(V) -> 
         (V == true -> reduce_stmt(config(S1, Env), Env1) ; Env1 = Env)
-    ;
-        throw(type_mismatch(E))
-    ).
+    ; throw(type_mismatch(E))
+    ), !.
 
 % Reduce an assignment statement
 reduce_stmt(config(assign(I, E1), Env), Env1) :-
     lookup(Env, I, id(I, _, DeclaredType)),
     reduce_all(config(E1, Env), config(V, Env)),
     get_type_expression(Env, V, ValueType),
-    DeclaredType = ValueType, !,
-    update_var(I, V, Env, Env1).
+    DeclaredType = ValueType,
+    update_var(I, V, Env, Env1), !.
 
 reduce_stmt(config(assign(I, V), _), _) :-
-    throw(type_mismatch(assign(I, V))).
+    throw(type_mismatch(assign(I, V))), !.
 
 % Handle call statements, including built-in and user-defined functions
 reduce_stmt(config(call(Func, Args), Env), Env) :-
     (is_builtin(Func) -> 
         handle_builtin(Func, Args, Env); 
-        handle_user_defined(Func, Args, Env)).
+        handle_user_defined(Func, Args, Env)), !.
 
 % Handle built-in functions generically
 handle_builtin(Func, [X], Env) :-
@@ -343,11 +336,11 @@ handle_user_defined(Func, Args, Env) :-
     lookup(Env, Func, id(Func, proc, proc(Params, Body))),
     create_env(Params, env([], 0, 0), LocalEnv),
     bind_args(Params, Args, LocalEnv, Env1),
-    reduce_stmt(config(Body, env(Env1, 0, 0)), _).
+    reduce_stmt(config(Body, env(Env1, 0, 0)), _), !.
 
 % Bind arguments to parameters in the local environment
-bind_args([], [], Env, Env).
+bind_args([], [], Env, Env) :- !.
 bind_args([param(P, _) | Params], [Arg | Args], LocalEnv, Env) :-
     reduce_all(config(Arg, Env), config(Value, Env)),
     create_env([var(P, Value)], LocalEnv, Env1),
-    bind_args(Params, Args, Env1, Env).
+    bind_args(Params, Args, Env1, Env), !.
